02a6ee709b7aeb723341cd360afacf18bf1b2dc7
Diego Llopis <diego.llopis@fornecedores.sicoob.com.br>
2025-02-15 17:44:06 -0300

    refactor: add info messages for each git step

--- git_actions.py
+++ git_actions.py
@@ -13,24 +13,27 @@ class GitAction:
         response = subprocess.run(['git', 'rev-parse', '--git-dir'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return response.returncode == 0 
     
+    def check_changed_files(self):
+        response = subprocess.run(['git', 'ls-files', '-m', '-o', '--exclude-from=.gitignore'], capture_output=True, text=True)
+        files_changed_list = response.stdout.splitlines()
+        return len(files_changed_list) > 0
+    
     def get_current_branch(self):
         branch_name = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode("utf-8")
         return branch_name
 
     def add_files(self):
         self.execute(['git', 'add', "."])
+        print('\nFiles ready to commit!\n')
 
     def commit(self, message: str):
         self.execute(['git', 'commit', '-m', message])
-
-    def check_changed_files(self):
-        response = subprocess.run(['git', 'ls-files', '-m', '-o', '--exclude-from=.gitignore'], capture_output=True, text=True)
-        files_changed_list = response.stdout.splitlines()
-        return len(files_changed_list) > 0
+        print('\nCommit setup done!\n')
 
     def push(self):
         branch_name = self.get_current_branch()
         self.execute(['git', 'push', '-u', 'origin', branch_name])
+        print('\nPush to remote repo done successfully!\n')
     
     def do_git_steps(self, message: str):
         self.add_files()
