6a6c8780f50c26f7d5158a1c97189d53666bd4b0
Diego Llopis <diego.llopis@fornecedores.sicoob.com.br>
2025-02-17 15:45:39 -0300

    feat: finish neuromancer splash and go version

--- classes/controller.py
+++ classes/controller.py
@@ -1,51 +1,54 @@
 import sys
 from classes.helper import Helper
-from classes.git_actions import GitAction
+from classes.git_action import GitAction
 
 class Controller:
 
-    commit_option: str = ''
-    commit_message: str = ''
-    items = ('feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore')
+    @classmethod
+    def format_commit_message(cls, option: str, message: str):
+        return f'{option}: {message}'
 
     @classmethod
-    def format_commit_message(cls):
-        return f'{cls.commit_option}: {cls.commit_message}'
+    def print_message(cls, text: str):
+        f'\n{text}\n'
 
     @classmethod
     def show_helper(cls):
         Helper.print_helper()
 
     @classmethod
+    def confirm_message_and_do_git_steps_or_cancel(cls, message: str):
+        confirm_options = ['n', 'y']
+
+        confirm = input(f'\n"{message}". Confirm? [y/n]: ').lower()
+        while confirm not in confirm_options:
+            confirm = input('\nWrong input. Try again [y/n]: ').lower()
+        if confirm == 'y':
+            GitAction.do_git_steps(message)
+        else:
+            cls.print_message('Operation cancelled.')
+
+    @classmethod
     def run(cls):
+        items = ('feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore')
+
         is_git = GitAction.check_is_repo_git()
         are_changes = GitAction.check_changed_files()
         
         if not is_git:
-            print('\nGit not found!\n')
+            cls.print_message('Git not found!')
             return
 
         if not are_changes:
-            print('\nThere are no modifications in this repository!\n')
+            cls.print_message('There are no modifications in this repository!')
             return
 
         if len(sys.argv) > 1:
             if sys.argv[1] == 'help':
                 cls.show_helper()
-            elif sys.argv[1] not in cls.items:
-                print(f'\nCommit option is not valid! Only options below are available.\n\n{cls.items}\n')
+            elif sys.argv[1] not in items:
+                print(f'\nCommit option is not valid! Only options below are available.\n\n{items}\n')
             else:
-                cls.commit_option = sys.argv[1]
-                cls.commit_message = ' '.join(sys.argv[2:])
-                message_formatted = cls.format_commit_message()
-                confirm = input(f'\n"{message_formatted}". Confirm? [y/n]: ').lower()
-                while confirm != 'y' and confirm != 'n':
-                    confirm = input('\nWrong input. Try again [y/n]: ').lower()
-                if confirm == 'y':
-                    GitAction.do_git_steps(message_formatted)
-                else:
-                    print()
-        else:
-            print('\nPlease add a commit message!\n')
-            return
+                commit_message = cls.format_commit_message(option= sys.argv[1], message= ' '.join(sys.argv[2:]))
+                cls.confirm_message_and_do_git_steps_or_cancel(message= commit_message)
             
