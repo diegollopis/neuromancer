7e232f457a3cf81d59d8947a86396f9727c8fbbe
Diego Llopis <diego.llopis@fornecedores.sicoob.com.br>
2025-02-16 22:28:47 -0300

    feat: add repo important infos

--- classes/git_actions.py
+++ classes/git_actions.py
@@ -10,18 +10,28 @@ class GitAction:
 
     @classmethod
     def check_is_git_detected(cls):
-        response = subprocess.run(['git', 'rev-parse', '--git-dir'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        response = subprocess.run(
+            ['git', 'rev-parse', '--git-dir'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
         return response.returncode == 0 
 
     @classmethod
     def check_changed_files(cls):
-        response = subprocess.run(['git', 'ls-files', '-m', '-o', '--exclude-from=.gitignore'], capture_output=True, text=True)
+        response = subprocess.run(
+            ['git', 'ls-files', '-m', '-o', '--exclude-from=.gitignore'],
+            capture_output=True,
+            text=True
+        )
         files_changed_list = response.stdout.splitlines()
         return len(files_changed_list) > 0
 
     @classmethod
     def get_current_branch(cls):
-        branch_name = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode("utf-8")
+        branch_name = subprocess.check_output(
+            ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+        ).strip().decode("utf-8")
         return branch_name
 
     @classmethod
@@ -30,7 +40,7 @@ class GitAction:
         print('\nFiles ready to commit!\n')
 
     @classmethod
-    def commit(self, message: str):
+    def commit(cls, message: str):
         GitAction.execute(['git', 'commit', '-m', message])
         print('\nCommit setup done!\n')
 
--- classes/utils.py
+++ classes/utils.py
@@ -16,9 +16,28 @@ class Utils:
         timer(1)
 
     @classmethod
+    def print_current_directory(cls):
+        cmd('pwd')
+
+    @classmethod
+    def print_current_branch(cls):
+        cmd('git branch --show-current')
+
+    @classmethod
+    def print_repo_remote_address(cls):
+        cmd('git config --get remote.origin.url')
+
+    @classmethod
+    def print_repo_infos(cls):
+        cls.print_current_directory()
+        cls.print_repo_remote_address()
+        cls.print_current_branch()
+
+    @classmethod
     def print_options(cls):
         title = 'NEUROMANCER 1.0'
         items = ('feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'help', 'quit')
+        cls.print_repo_infos()
         cls.print_line(len(title))
         print(title)
         cls.print_line(len(title))
