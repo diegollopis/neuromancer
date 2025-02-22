c3f7413f51a150690175f69a258b0717b5a9755f
Diego Llopis <diego.llopis@fornecedores.sicoob.com.br>
2025-02-22 18:47:36 -0300

    chore: add semantic commit references

--- classes/helper.py
+++ classes/helper.py
@@ -21,10 +21,20 @@ feat: add hat wobble
         'chore' : '(updating grunt tasks etc; no production code change)'
     }
 
+    references = [
+        'www.conventionalcommits.org/',
+        'seesparkbox.com/foundry/semantic_commit_messages',
+        'karma-runner.github.io/1.0/dev/git-commit-msg.html'
+    ]
+
     @classmethod
     def print_helper(cls):
         cmd('clear')
         print(f'{cls.commit_message_example}\n')
         for key, value in cls.commit_message_types.items():
             print(f'{key}: {value}')
+        print()
+        print('References\n')
+        for reference in cls.references:
+            print(reference)
         print()
